e305bbc4d95b0823f9ede1efdf45ff7359c9f277
Maria Mersalin <mersalin@outlook.com>
2020-09-25 20:18:37 +0000

    Sync LeetCode submission - Largest Number (java)

---
+++ my-folder/problems/largest_number/solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+  public String largestNumber(int[] nums) {
+    if (nums == null || nums.length == 0) return "";
+    String[] strs = new String[nums.length];
+    for (int i = 0; i < nums.length; i++) {
+      strs[i] = nums[i] + "";
+    }
+    Arrays.sort(strs, (i, j) ->{
+      String s1 = i + j;
+      String s2 = j + i;
+      return s1.compareTo(s2);
+    });
+    if (strs[strs.length - 1].charAt(0) == '0') return "0";
+    String res = new String();
+    for (int i = 0; i < strs.length; i++) {
+      res = strs[i] + res;
+    }
+    return res;
+  }
+}
