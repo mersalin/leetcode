be74177608aaaf415eeda052329fa2681c6032a8
Maria Mersalin <mersalin@outlook.com>
2020-09-25 19:42:01 +0000

    Sync LeetCode submission - Check If It Is a Straight Line (java)

--- my-folder/problems/check_if_it_is_a_straight_line/solution.java
+++ my-folder/problems/check_if_it_is_a_straight_line/solution.java
@@ -1,15 +1,22 @@
 class Solution {
-    public boolean checkStraightLine(int[][] coordinates) {
-        int x0 = coordinates[0][0], y0 = coordinates[0][1];
-        int x1 = coordinates[1][0], y1 = coordinates[1][1];
-        int dx = x1 - x0, dy = y1 - y0;
-        
-        for (int[] co : coordinates) {
-            int x = co[0], y = co[1];
-            if (dx * (y - y0) != dy * (x - x0))
-                return false;
-        }
-        
-        return true; 
+  public boolean checkStraightLine(int[][] coordinates) {
+    int x1 = coordinates[0][0];
+    int y1 = coordinates[0][1];
+    int x2 = coordinates[1][0];
+    int y2 = coordinates[1][1];
+
+    int dx = (x2 - x1);
+    int dy = (y2 - y1);
+
+    int n = coordinates.length;
+    for (int i = 1; i < n; i++) {
+      int x = coordinates[i][0];
+      int y = coordinates[i][1];
+
+      if (dy * (x - x1) != dx * (y - y1)) {
+        return false;
+      }
     }
+    return true;
+  }
 }
