95afa2e07ffe567fcc1878cdc167add76c50a8f2
Maria Mersalin <mersalin@outlook.com>
2020-09-25 19:50:04 +0000

    Sync LeetCode submission - Valid Perfect Square (java)

--- my-folder/problems/valid_perfect_square/solution.java
+++ my-folder/problems/valid_perfect_square/solution.java
@@ -1,16 +1,11 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        long left = 1, right = num;
-        while (left <= right) {
-            long mid = (left + right) / 2;
-            if (mid * mid == num){
-                 return true;
-            } else if (mid * mid < num) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return false;
+	int i = 1;
+	while (num > 0) {
+		num = num - i;
+		i = i + 2;
+	}
+	return num == 0;
     }
 }
+
