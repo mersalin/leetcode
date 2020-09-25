6f9bf3ec07f8d241645f4d1b64724fb66b2f53e6
Maria Mersalin <mersalin@outlook.com>
2020-09-25 16:58:56 +0000

    Sync LeetCode submission - Jewels and Stones (java)

--- my-folder/problems/jewels_and_stones/solution.java
+++ my-folder/problems/jewels_and_stones/solution.java
@@ -1,21 +1,16 @@
 class Solution {
     public int numJewelsInStones(String J, String S) {
-        
-        Set<Character> jewelChars = new HashSet(J.length());
-        
-         for(int i = 0; i < J.length(); i++) {
-             jewelChars.add(J.charAt(i));
-         }
-        
-        
-        int jewels = 0;
-        
-        for(int i = 0; i < S.length(); i++) {
-            if(jewelChars.contains(S.charAt(i))) {
-                jewels++;
-            }  
-        }
-        return jewels;
-        
+	
+	Map<Character, Integer> map = new HashMap<>();
+
+	for (char c : S.toCharArray()) {
+		map.put(c, map.getOrDefault(c, 0) + 1);
+}
+
+int jewelCount = 0;
+for (char c : J.toCharArray()) {
+	jewelCount = jewelCount + map.getOrDefault(c, 0);
+}
+        return jewelCount;
     }
 }
