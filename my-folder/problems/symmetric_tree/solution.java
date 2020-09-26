9b64fd594b9671610e31261430dd243ccb64d8f0
Maria Mersalin <mersalin@outlook.com>
2020-09-26 17:41:29 +0000

    Sync LeetCode submission - Symmetric Tree (java)

--- my-folder/problems/symmetric_tree/solution.java
+++ my-folder/problems/symmetric_tree/solution.java
@@ -1,33 +1,58 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        return isSymmetric(root, root);
+  public boolean isSymmetric(TreeNode root) {
+    if (root == null) {
+      return true;
     }
-    
-    
-    private boolean isSymmetric(TreeNode t1, TreeNode t2) {
-        if(t1 == null && t2 == null) {
-            return true;
-        }
-        
-        if (t1 == null || t2 == null) {
+
+    Queue < TreeNode > queue = new LinkedList < >();
+    if (root.right != null && root.left != null) {
+      queue.offer(root.left);
+      queue.offer(root.right);
+    } else if (root.left == null && root.right == null) {
+      return true;
+    } else {
+      return false;
+    }
+
+    while (!queue.isEmpty()) {
+      int n = queue.size();
+      if (n % 2 != 0) {
+        return false;
+      }
+      Queue < TreeNode > q = new LinkedList < >();
+      Stack < TreeNode > stack = new Stack < >();
+      for (int i = 0; i < n / 2; i++) {
+        TreeNode tNode = queue.poll();
+        q.offer(tNode);
+        insert(queue, tNode);
+      }
+      for (int i = n / 2; i < n; i++) {
+        TreeNode tNode = queue.poll();
+        stack.push(tNode);
+        insert(queue, tNode);
+      }
+
+      for (int i = 0; i < n / 2; i++) {
+        TreeNode n1 = q.poll();
+        TreeNode n2 = stack.pop();
+        if (n1 == null && n2 != null) {
             return false;
+        } else if (n2 == null && n1 != null) {
+          return false;
+        } else if (n1 == null && n2 == null) {
+          continue;
+        } else if (n1.val != n2.val) {
+          return false;
         }
-        
-        return t1.val == t2.val && isSymmetric(t1.right, t2.left) && isSymmetric(t1.left, t2.right);
+      }
     }
+    return true;
+  }
+
+  private void insert(Queue < TreeNode > q, TreeNode node) {
+    if (node != null) {
+      q.offer(node.right);
+      q.offer(node.left);
+    }
+  }
 }
