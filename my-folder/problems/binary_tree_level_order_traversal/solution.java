66306eb7db3cddfdcb1f589a5d35880289000d1c
Maria Mersalin <mersalin@outlook.com>
2020-09-26 17:52:05 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (java)

--- my-folder/problems/binary_tree_level_order_traversal/solution.java
+++ my-folder/problems/binary_tree_level_order_traversal/solution.java
@@ -1,44 +1,29 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        if (root == null) {
-            return Collections.emptyList();
-        }
-        List<List<Integer>> result = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
-        q.offer(root);
-        while (!q.isEmpty()) {
-            List<Integer> list = new ArrayList<>();
-            int n = q.size();
-            for (int i=0; i<n; i++) {
-                TreeNode node = q.poll();
-                list.add(node.val);
-                if(node.left != null) {
-                    q.offer(node.left);
-                }
-                if(node.right != null) {
-                    q.offer(node.right);
-                }
-            }
-            if(list.size() > 0) {
-                result.add(list);
-            }
-        }
-        return result;
+  public List < List < Integer >> levelOrder(TreeNode root) {
+    List < List < Integer >> ans = new ArrayList < >();
+
+    if (root == null) {
+      return ans;
     }
+
+    Queue < TreeNode > queue = new LinkedList < >();
+    queue.offer(root);
+
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      List < Integer > list = new ArrayList < >();
+      for (int i = 0; i < size; i++) {
+        TreeNode node = queue.poll();
+        if (node != null) {
+          list.add(node.val);
+          queue.offer(node.left);
+          queue.offer(node.right);
+        }
+      }
+      if (list.size() > 0) {
+        ans.add(list);
+      }
+    }
+    return ans;
+  }
 }
