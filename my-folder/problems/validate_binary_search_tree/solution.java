5cecd9147b134e115c14defd0ba46a6e647636cc
Maria Mersalin <mersalin@outlook.com>
2020-09-26 16:51:00 +0000

    Sync LeetCode submission - Validate Binary Search Tree (java)

--- my-folder/problems/validate_binary_search_tree/solution.java
+++ my-folder/problems/validate_binary_search_tree/solution.java
@@ -1,45 +1,30 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isValidBST(TreeNode root) {
         return isValidBST(root, null, null);
     }
-    
-    private boolean isValidBST(TreeNode node, Integer min, Integer max) {
-        if (node == null) {
-            return true;
-        }
-        
-        int val = node.val;
-        
-        if (min != null && val <= min) {
-            return false;
-        }
-        if (max != null && val >= max) {
-            return false;
-        }
-        
-        if (!isValidBST(node.right, val, max)) {
-            return false;
-        }
-        
-        if (!isValidBST(node.left, min, val)) {
-            return false;
-        }
-        
-        return true;
-    }
+
+	private boolean isValidBST(TreeNode node, Integer min, Integer max) {
+		if (node == null) {
+			return true;
+		}
+
+		int val = node.val;
+		
+		if (min != null && val <= min) {
+			return false;
+		}
+
+		if (max != null && val >= max) {
+			return false;
+		}
+
+		if(!isValidBST(node.right, val, max)) {
+			return false;
+		}
+
+		if(!isValidBST(node.left, min, val)) {
+			return false;
+		}
+		return true;
+	}
 }
