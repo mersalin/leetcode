b420d2fee8b4d3f46572a6f9d3f795dcb00a3c29
Maria Mersalin <mersalin@outlook.com>
2020-10-14 13:52:29 +0000

    Sync LeetCode submission - House Robber II (java)

---
+++ my-folder/problems/house_robber_ii/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return nums[0];
+        }
+        
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        
+        for (int i=2; i<=n; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i-1]);
+        }
+        int withOutLastHouse = dp[n-1];
+        
+        dp[1] = 0;
+        for (int i=2; i<=n; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i-1]);
+        }
+        
+        int withOutFirstHouse = dp[n];
+        return Math.max(withOutFirstHouse, withOutLastHouse); 
+    }
+}
