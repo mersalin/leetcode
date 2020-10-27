b0048a1568135aef1fee3a080e534a53432f95a7
Maria Mersalin <mersalin@outlook.com>
2020-10-27 10:48:16 +0000

    Sync LeetCode submission - Edit Distance (java)

---
+++ my-folder/problems/edit_distance/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int row = word1.length();
+        int col = word2.length();
+        
+        int[][] dp = new int[row+1][col+1];
+        
+        for (int i = 1; i <= row; i++) {
+            dp[i][0] = i;
+        }
+        
+        for (int j = 1; j <= col; j++) {
+            dp[0][j] = j;
+        }
+        
+        for (int i=1; i<=row; i++) {
+            for (int j=1; j<=col; j++) {
+                if (i <= row && j <= col && word1.charAt(i-1) == word2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1])) + 1;
+                }
+            }
+        }
+        return dp[row][col];
+    }
+}
