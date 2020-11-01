011035e8a4d18f058bda53d9a4bdeadd9cfeb327
Maria Mersalin <mersalin@outlook.com>
2020-11-01 15:01:41 +0000

    Sync LeetCode submission - Meeting Rooms (java)

---
+++ my-folder/problems/meeting_rooms/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        int n = intervals.length;
+        
+        if (n == 0) {
+            return true;
+        }
+        
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+        
+        for (int i=1; i<n; i++) {
+            if (intervals[i-1][1] > intervals[i][0]) {
+                return false;
+            } 
+        }
+        return true;
+    }
+}
