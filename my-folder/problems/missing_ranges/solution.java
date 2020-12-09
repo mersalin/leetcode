ff794f66130f30200a6bcd079050bd97c979b551
Maria Mersalin <mersalin@outlook.com>
2020-12-09 07:43:39 +0000

    Sync LeetCode submission - Missing Ranges (java)

---
+++ my-folder/problems/missing_ranges/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+      List<String> res = new ArrayList<String>();
+      int next = lower;
+
+      for (int i = 0; i < nums.length; i++) {
+        if (nums[i] < next) continue;
+        if (nums[i] == next) {
+          next++;
+          continue;
+        }
+        res.add(getRange(next, nums[i] - 1));
+        next = nums[i] + 1;
+      }
+
+      if (next <= upper) res.add(getRange(next, upper));
+
+      return res;
+    }
+
+    private String getRange(int n1, int n2) {
+      return (n1 == n2) ? String.valueOf(n1) : String.format("%d->%d", n1, n2);
+    }
+}
