50d69c0dd8392af34ecc048753e27a6eb8ebc6ec
Maria Mersalin <mersalin@outlook.com>
2022-06-22 18:12:10 +0000

    Sync LeetCode submission - Valid Sudoku (java)

--- my-folder/problems/valid_sudoku/solution.java
+++ my-folder/problems/valid_sudoku/solution.java
@@ -1,20 +1,27 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        for(int i = 0; i<9; i++){
-            HashSet<Character> rows = new HashSet<Character>();
-            HashSet<Character> columns = new HashSet<Character>();
-            HashSet<Character> cube = new HashSet<Character>();
-            for (int j = 0; j < 9;j++){
-                if(board[i][j]!='.' && !rows.add(board[i][j]))
+        
+        Set<String> seen = new HashSet<>();
+        
+        int n = board.length;
+        int m = board[0].length;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+                
+                char number = board[i][j];
+                
+                if (!seen.add("seen " + number + "in row " + i) || 
+                    !seen.add("seen " + number + "in col " + j) || 
+                    !seen.add("seen " + number + "in board " + i/3 + j/3)) {
                     return false;
-                if(board[j][i]!='.' && !columns.add(board[j][i]))
-                    return false;
-                int RowIndex = 3*(i/3);
-                int ColIndex = 3*(i%3);
-                if(board[RowIndex + j/3][ColIndex + j%3]!='.' && !cube.add(board[RowIndex + j/3][ColIndex + j%3]))
-                    return false;
+                }    
             }
         }
+
         return true;
     }
 }
