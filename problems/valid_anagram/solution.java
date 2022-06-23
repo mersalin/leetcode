43ace3383480cfb6894fc5bb1c35a189f8b67f52
Maria Mersalin <mersalin@outlook.com>
2022-06-23 17:16:00 +0000

    Sync LeetCode submission - Valid Anagram (java)

--- problems/valid_anagram/solution.java
+++ problems/valid_anagram/solution.java
@@ -1,21 +1,27 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-      
-      int[] letters = new int[26];
-      
-      for (char c : s.toCharArray()) {
-        letters[c - 'a']++;
-      }
-      
-      for (char c : t.toCharArray()) {
-        letters[c - 'a']--;
-      }
-      
-      for (int i : letters) {
-        if (i>0 || i <0) {
-          return false;
+        int n = s.length();
+        int m = t.length();
+        
+        if (n != m)  {
+            return false;
         }
-      }
+        
+        int[] letters = new int[26];
+        
+        for (int i = 0; i < n; i++) {
+            letters[s.charAt(i) - 'a']++;
+        }
+        
+        for (int i = 0; i < m; i++) {
+            letters[t.charAt(i) - 'a']--;
+        }
+        
+        for (int i = 0; i < 26; i++) {
+            if (letters[i] > 0 || letters[i] < 0) {
+                return false;
+            }
+        }
         return true;
     }
 }
