814002aea804591c1fbd3262406ab700e7478a2f
Maria Mersalin <mersalin@outlook.com>
2022-06-23 17:03:14 +0000

    Sync LeetCode submission - Ransom Note (java)

--- problems/ransom_note/solution.java
+++ problems/ransom_note/solution.java
@@ -1,24 +1,25 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-int r = ransomNote.length();
-	int m = magazine.length();
-
-	if (r > m) {
-		return false;
-	}
-
-	int[] letters = new int[26];
-
-	for (int i=0; i<m; i++) {
-		letters[magazine.charAt(i) - 'a']++;
-	}
-
-	for (int i=0; i<r; i++) {
-		letters[ransomNote.charAt(i) - 'a']--;
-		if(letters[ransomNote.charAt(i) - 'a'] < 0) {
-			return false;
-		}
-	}
-	return true;
+        int n = ransomNote.length();
+        int m = magazine.length();
+        
+        if (n > m) {
+            return false;
+        }
+        
+        int[] letters = new int[26];
+        
+        for (int i = 0; i < m; i++) {
+            letters[magazine.charAt(i) - 'a']++;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            letters[ransomNote.charAt(i) - 'a']--;
+            
+            if (letters[ransomNote.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
