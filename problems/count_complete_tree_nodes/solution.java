dbbe4fa465f49453402d330ce07e56b3c56dda28
Maria Mersalin <mersalin@outlook.com>
2024-01-09 18:34:09 +0000

    Sync LeetCode submission - Count Complete Tree Nodes (java)

--- problems/count_complete_tree_nodes/solution.java
+++ problems/count_complete_tree_nodes/solution.java
@@ -1,21 +1,42 @@
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public int countNodes(TreeNode root) {
         if (root == null) {
-          return 0;
+            return 0;
         }
-      
-        int nodes = 0;
+        
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-        while(!queue.isEmpty()) {
-          TreeNode node = queue.poll();
-          if (node != null) {
-            nodes++;
-            queue.offer(node.right);
-            queue.offer(node.left);
-          }
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            int queueSize = queue.size();
+            count = count + queueSize;
+
+            for (int i=0; i<queueSize; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
         }
-      return nodes;
+        return count;
     }
 }
